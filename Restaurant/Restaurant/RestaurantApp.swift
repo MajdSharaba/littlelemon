20d22a9728d22ca2eb0896a8a83e49de553207a5
Ahmad <husseinalabdally755@gmail.com>
2023-03-17 16:05:58 +0300

    First commit

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by YAWAR on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
